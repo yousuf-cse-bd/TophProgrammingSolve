1a22b8fda7267496d7a361d841dd793714404a11
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-05 07:02:28 +0600

    Just sum them

---
+++ 2_AddThemUpToph.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file 2_AddThemUpToph.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Read two integers, calculate sum of them
+ * @version 0.1
+ * @date 2021-07-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n1, n2;
+	cin>>n1>>n2;
+	n1 = n1 + n2;
+	cout<<n1<<endl;
+	return 0;
+}
