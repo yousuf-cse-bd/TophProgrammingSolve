1107bb527a38ba91cc9c9be0c50a40e976bc6966
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-05 06:58:42 +0600

    Input Output

---
+++ 1_CopycatToph.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file 1_CopycatToph.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Read an integer variable and print it.
+ * @version 0.1
+ * @date 2021-07-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin>>n;
+	cout<<n<<endl;
+
+	return 0;
+}
