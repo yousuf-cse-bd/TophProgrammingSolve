fb1bd4ce04e0d5be449e1824f7f774602d17e1fb
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-19 05:17:26 +0600

    Just squared a integer value

---
+++ 24_SquaredToph.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file 24_SquaredToph.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Just squared a integer value
+ * @version 0.1
+ * @date 2022-07-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int l;
+    cin>>l;
+    unsigned int squared = l*l;
+    cout<<squared<<endl;
+    return 0;
+}
