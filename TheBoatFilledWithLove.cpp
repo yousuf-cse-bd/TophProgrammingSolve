86e077582b44ceaadc97f012ece75f0905b12aa6
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-02-16 23:54:43 +0600

    Tiny problem just console print a unsigned int with string literal

---
+++ TheBoatFilledWithLove.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file TheBoatFilledWithLove.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Tiny problem just console print a unsigned int with string literal
+ * @version 0.1
+ * @date 2023-02-16
+ * @since ThursDay; 11:53 PM
+ * @copyright Copyright (c) 2023
+ * @institute: Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    unsigned int n;
+    cin>>n;
+    cout<<"We Love You. We "<<n<<" Are Here For You."<<endl;
+    return 0;
+}
